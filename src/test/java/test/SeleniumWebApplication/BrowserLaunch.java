d80dc1199f61bf6ed71a7ea3976134670a32ef55
Naveen-mmm <naveenkumar.tsm@gmail.com>
2024-12-17 15:01:32 +0530

    BrowserLaunch-Second

--- src/test/java/test/SeleniumWebApplication/BrowserLaunch.java
+++ src/test/java/test/SeleniumWebApplication/BrowserLaunch.java
@@ -2,11 +2,13 @@ package test.SeleniumWebApplication;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class BrowserLaunch {
 	
 	public void browser()
 	{
-		WebDriver driver = new EdgeDriver();
+		//WebDriver driver = new EdgeDriver();
+		WebDriver driver = new ChromeDriver();
 	}
 
 	public static void main(String[] args) {
