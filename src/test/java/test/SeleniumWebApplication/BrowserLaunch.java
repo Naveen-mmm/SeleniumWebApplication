d29c8bc440d4e21275eae629bccea1c6953f121e
Naveen-mmm <naveenkumar.tsm@gmail.com>
2024-12-17 14:49:08 +0530

    BrowserLaunch

---
+++ src/test/java/test/SeleniumWebApplication/BrowserLaunch.java
@@ -0,0 +1,20 @@
+package test.SeleniumWebApplication;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.edge.EdgeDriver;
+public class BrowserLaunch {
+	
+	public void browser()
+	{
+		WebDriver driver = new EdgeDriver();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BrowserLaunch obj = new BrowserLaunch();
+		obj.browser();
+
+	}
+
+}
